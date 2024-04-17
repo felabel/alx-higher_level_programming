e8dd8a614f8234ffa3002386ff7ade521744c4a4
Felicity Essien <felicityabel2016@gmail.com>
2024-04-16 23:55:34 -0700

    another correction

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""script that lists all State objects that contain
- the letter a from the database hbtn_0e_6_usa"""
+"""script that prints the first State object from the database hbtn_0e_6_usa"""
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import Session
@@ -8,12 +7,19 @@ from sqlalchemy import create_engine
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    q = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
-    for state in q:
+    
+    # Query the first State object ordered by id
+    state = session.query(State).order_by(State.id).first()
+    
+    if state is None:
+        print("Nothing")
+    else:
         print("{}: {}".format(state.id, state.name))
+    
     session.close()
+
