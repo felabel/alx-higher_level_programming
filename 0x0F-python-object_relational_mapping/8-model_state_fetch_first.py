a1e97e0431686d90b9f855ad385898b2a08fda95
Felicity Essien <felicityabel2016@gmail.com>
2024-04-16 23:51:07 -0700

    task 8 correction

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
-"""
+"""script that prints the first State object from the database hbtn_0e_6_usa"""
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    instance = session.query(State).first()
-    if instance is None:
-        print("Nothing")
+    session = Session(engine)
+    s = session.query(State).order_by(State.id).one()
+    if s:
+        print("{}: {}".format(s.id, s.name))
     else:
-        print(instance.id, instance.name, sep=": ")
+        print("Nothing")
+    session.close()
